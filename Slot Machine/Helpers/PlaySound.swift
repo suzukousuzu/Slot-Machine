59aa039b59c1257dbb372b9f53eb47e2f1d61048
Kota Suzuki <addressuzuki@gmail.com>
2024-03-13 19:50:24 +0900

    次のコミット

---
+++ Slot Machine/Helpers/PlaySound.swift
@@ -0,0 +1,8 @@
+//
+//  PlaySound.swift
+//  Slot Machine
+//
+//  Created by 鈴木航太 on 2024/03/13.
+//
+
+import Foundation
---
+++ Slot Machine/Views/InfoView.swift
@@ -0,0 +1,20 @@
+//
+//  InfoView.swift
+//  Slot Machine
+//
+//  Created by 鈴木航太 on 2024/03/10.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
